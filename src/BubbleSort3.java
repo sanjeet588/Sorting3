1d5b88cbaebadec05b94097559fd4e4769185fba
Sanjeet Nayak <sanjeetnayak@localhost>
2021-02-03 09:50:01 +0530

    bubble sort

---
+++ src/BubbleSort3.java
@@ -0,0 +1,12 @@
+
+public class BubbleSort3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bubble Sort Begin");
+		int a = 30;
+		System.out.println(a);
+		System.out.println("Bubble Sort Ends");
+	}
+
+}
